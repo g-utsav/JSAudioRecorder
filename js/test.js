f092527663d4df190775f04542ba96b6ddddd567
Utsav Gupta <gutsav9@gmail.com>
2024-03-06 20:12:01 +0530

    Android Tost test

--- js/test.js
+++ js/test.js
@@ -1,9 +1,6 @@
 function msg(){
-       
-    var fname=document.getElementById("fname").value;
-    var pswd=document.getElementById("pswd").value;
  
     // Dialog is javascript interface name
     // mentioned in Mainactivity.Java
-    Dialog.showMsg(fname,pswd);
+    Dialog.showMsg("Test Fname","Test Lname");
     }
