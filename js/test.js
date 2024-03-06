d9f0495adb9273f7acdafe5fad4c34a8afed2898
Utsav Gupta <gutsav9@gmail.com>
2024-03-06 20:18:43 +0530

    Android Tost test

--- js/test.js
+++ js/test.js
@@ -2,5 +2,7 @@ function msg(){
  
     // Dialog is javascript interface name
     // mentioned in Mainactivity.Java
-    Android.showMsg("Test Fname","Test Lname");
+    // Android.showMsg("Test Fname","Test Lname");
+    let ret = Android.playSound("This is tost")
+    console.log("this is from js "+ret);
     }
